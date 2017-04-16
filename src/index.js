0f9f9bc08ac5294a8eb347ea428e9ed89bc9dda5
Jacob Meacham <jacob.e.meacham@gmail.com>
2017-04-15 17:57:12 -0700

    Add tests.

--- src/index.js
+++ src/index.js
@@ -4,48 +4,64 @@ import childProcess from 'child_process'
 
 const GIT_PREFIX = 'git'
 
-const resolvedValues = {}
-
-async function getValueFromGit(variable) {
-  if (resolvedValues[variable]) {
-    return resolvedValues[variable]
-  }
-
-  function exec(cmd, options = { timeout: 1000 }) {
-    return new Promise((resolve, reject) => {
-      childProcess.exec(cmd, options, (err, stdout, stder) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(stdout)
-        }
-      })
+async function _exec(cmd, options = { timeout: 1000 }) {
+  return new Promise((resolve, reject) => {
+    childProcess.exec(cmd, options, (err, stdout) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(stdout.trim())
+      }
     })
-  }
-
-  switch (variable) {
-    case 'describe':
-      return exec('git describe')
-    case 'sha1':
-      return exec('git rev-parse --short')
-    case 'branch':
-      return exec('git rev-parse --abbrev-ref HEAD')
-    default:
-      throw new Error(`Git variable ${variable} is unknown. Candidates are 'describe', 'sha1', 'branch'`)
-  }
+  })
 }
 
 export default class ServerlessGitVariables {
   constructor(serverless, options) {
+    this.resolvedValues = {}
     const delegate = serverless.variables.getValueFromSource.bind(serverless.variables)
 
     serverless.variables.getValueFromSource = (variableString) => {
       if (variableString.startsWith(`${GIT_PREFIX}:`)) {
         const variable = variableString.split(`${GIT_PREFIX}:`)[1]
-        return deasyncPromise(getValueFromGit(variable))
+        return this._getValueFromGitSync(variable)
       }
 
       return delegate(variableString)
     }
+  }
+
+  _getValueFromGitSync(variable) {
+    if (this.resolvedValues[variable]) {
+      return this.resolvedValues[variable]
+    }
+
+    return deasyncPromise(this._getValueFromGit(variable))
+  }
+
+  async _getValueFromGit(variable) {
+    let value = null
+    switch (variable) {
+      case 'describe':
+        value = await _exec('git describe')
+        break
+      case 'sha1':
+        value = await _exec('git rev-parse --short HEAD')
+        break
+      case 'branch':
+        value = await _exec('git rev-parse --abbrev-ref HEAD')
+        break
+      default:
+        throw new Error(`Git variable ${variable} is unknown. Candidates are 'describe', 'sha1', 'branch'`)
+    }
+
+    // TODO: Figure out why if I don't log, the deasync promise
+    // never resolves. Catching it in the debugger or logging
+    // causes it to work fine.
+    process.stdout.write('')
+
+    // Cache before returning
+    this.resolvedValues[variable] = value
+    return value
   }
 }
